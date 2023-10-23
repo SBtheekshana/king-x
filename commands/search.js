0abd6e40e4d61b3a79ed58aae4917d6e74413a73
SBtheekshana <147326000+SBtheekshana@users.noreply.github.com>
2023-10-23 09:54:51 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -206,7 +206,29 @@ cmd({
   catch(e){  
   console.log(e)  
   }})
-//-----------------------------------------------------------------------
+//--------------------------------------------------------------------------
+cmd({  
+      pattern: "hiru",  
+      alias: ["8","hn"],  
+      react: "📰",  
+      desc: "hiru",  
+      category: "news",  
+      use: '.hirunews',  
+      filename: __filename  
+  },  
+  async(Void, citel) => {  
+  try{  
+  const nasa = await fetchJson(`https://queen-api.onrender.com/api/news/hiru-news`);  
+  
+            const images = `${nasa.result.image}`  
+             const title = `${nasa.result.title}` 
+             const news = `${nasa.result.description}`  
+  
+  await Void.sendMessage(citel.chat,  { image: { url: images }, caption: `\n*${ title }*\n\n _${news}._\n\n*`}, { quoted: citel })  
+  }  
+  catch(e){  
+  console.log(e)  
+  }})
 //--------------------------------------------------------------------------
 cmd({  
       pattern: "nasa",  
