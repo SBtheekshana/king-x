cfe6f1a52006cc32230080180c28f74261142fea
SBtheekshana <147326000+SBtheekshana@users.noreply.github.com>
2023-10-24 15:18:39 +0530

    Update misc.js

--- commands/misc.js
+++ commands/misc.js
@@ -52,7 +52,7 @@ cmd({
   },  
   async(Void, citel) => {  
   try{  
-  const response = await fetchJson(`https://queen-api.onrender.com/api/chatgpt/dalle-e-2?message=${citel.text}`);  
+  const response = await fetchJson(`https://queen-api.onrender.com/api/chatgpt/gpt-3?message=${citel.text}`);  
   
   const images = `${result.image}`  
   
@@ -155,7 +155,7 @@ cmd({
   },  
   async(Void, citel) => {  
   try{  
-  const response = await fetchJson(`https://queen-api.onrender.com/api/chatgpt/gpt-1?message=${citel.text}`);  
+  const response = await fetchJson(`https://queen-api.onrender.com/api/chatgpt/gpt-2?message=${citel.text}`);  
   
   const result = `${response.result}`  
   
