2cc0cb9e00762b2eab950e91c9e41970121c902c
SBtheekshana <147326000+SBtheekshana@users.noreply.github.com>
2023-10-25 12:29:34 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -201,13 +201,36 @@ cmd({
             const images = `${nasa.result.image}`  
              const title = `${nasa.result.title}` 
              const news = `${nasa.result.description}` 
-            const url =`${nasa. result. url} `
   
   await Void.sendMessage(citel.chat,  { image: { url: images }, caption: `\n*${ title }*\n\n _${news}._\n\n*`}, { quoted: citel })  
   }  
   catch(e){  
   console.log(e)  
   }})
+//--------------------------------------------------------------------------
+cmd({  
+      pattern: "wabeta",  
+      alias: ["05"],  
+      react: "📰",  
+      desc: "nasa",  
+      category: "news",  
+      use: '.hirunews',  
+      filename: __filename  
+  },  
+  async(Void, citel) => {  
+  try{  
+  const nasa = await fetchJson(`https://queen-api.onrender.com/api/news/wabeta-news`);  
+  
+             const images = `${nasa.result.image}`  
+             const title = `${nasa.result.title}` 
+             const news = `${nasa.result.description}` 
+             const url =`${nasa. result. url} `
+  
+  await Void.sendMessage(citel.chat,  { image: { url: images }, caption: `\n*${ title }*\n\n _${news}._\n\n*`}, { quoted: citel })  
+  }  
+  catch(e){  
+  console.log(e)  
+  }}) 
 //--------------------------------------------------------------------------
 cmd({  
       pattern: "esana",  
