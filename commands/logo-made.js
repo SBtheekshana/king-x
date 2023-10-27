a0d1d3d1de477796d41bb8fd7399628cea780395
SBtheekshana <147326000+SBtheekshana@users.noreply.github.com>
2023-10-27 10:14:16 +0530

    Update logo-made.js

--- commands/logo-made.js
+++ commands/logo-made.js
@@ -15,9 +15,9 @@ const prefix = Config.prefix
 const maker = require('@sl-code-lords/text-pro-me')
 
     //---------------------------------------------------------------------------
-cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+cmd({ pattern: "cartoon", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
         if (!text) return citel.reply('_Need text._')
-        let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', text)
+        let anu = await maker.textpro('https://textpro.me/3d-cartoon-text-effect-generator-online-1123.html', text)
         Void.sendMessage(citel.chat, { image: { url: anu }, caption: `Made by ${tlang().title},For my ${tlang().greet}` }, { quoted: citel })
     })
     //---------------------------------------------------------------------------
